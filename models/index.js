d2879106f925d17669fec40b8ef5e4c74c3c7dda
Erika Newland <erika.s.newland@gmail.com>
2017-08-07 11:34:04 -0400

    resave

--- models/index.js
+++ models/index.js
@@ -5,14 +5,21 @@ var db = new Sequelize('postgres://localhost:5432/wikistack', {
 
 const User=db.define('user', {
     name: {type: Sequelize.STRING,allowNull: false},
-    email: {type: Sequelize.STRING, unique: true, isEmail: true, allowNull: false}
+    email: {type: Sequelize.STRING, unique: true, validate: {isEmail: true}, allowNull: false}
 });
 
 const Page = db.define('page', {
     title: {type: Sequelize.STRING, allowNull: false},
     urlTitle: {type: Sequelize.STRING, isUrl: true, allowNull: false},
     content: {type: Sequelize.TEXT,allowNull: false},
-    status: Sequelize.ENUM('open', 'closed')
+    status: Sequelize.ENUM('open', 'closed'),
+    date: {type: Sequelize.DATE, defaultValue: Sequelize.NOW}
+}, {
+    getterMethods: {
+        route() {
+            return '/wiki/${this.urlTitle}'
+        }
+    }
 });
 
 module.exports={
